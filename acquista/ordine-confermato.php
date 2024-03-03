932d0dbb5dc04230c069e07feb4bf08c62249ac4
AlessioMozzino <82084197+AlessioMozzino@users.noreply.github.com>
2024-03-03 17:18:07 +0100

    Add files via upload

---
+++ acquista/ordine-confermato.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-133909830-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-133909830-1');
+</script>
+<link rel="apple-touch-icon" sizes="57x57" href="https://www.mozzino.it/favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="https://www.mozzino.it/favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="https://www.mozzino.it/favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="https://www.mozzino.it/favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="https://www.mozzino.it/favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="https://www.mozzino.it/favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="https://www.mozzino.it/favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="https://www.mozzino.it/favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="https://www.mozzino.it/favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="https://www.mozzino.it/favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://www.mozzino.it/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="https://www.mozzino.it/favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://www.mozzino.it/favicon/favicon-16x16.png">
+<link rel="manifest" href="https://www.mozzino.it/favicon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="https://www.mozzino.it/favicon/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Il tuo ordine è stato confermato: grazie per aver scelto il mio servizio</title>
+	<meta name="keywords" content="documenti catastali, catasto, planimetria catastale, visura planimetrica, visura per immobile, estratto di mappa, visura storica, sister, agenzia delle entrate, visura catastale online, planimetria catastale online, mappa catastale, mappa, agenzia del territorio">
+	<meta name="description" content="Il tuo ordine è stato confermato: grazie per aver scelto il mio servizio.">
+    <!-- Fogli di stile bootstrap -->
+	<link rel="stylesheet" type="text/css" href="./../css/bootstrap/bootstrap.min.css">
+	<!-- Fogli di stile personali -->
+	<link rel="stylesheet" type="text/css" href="./../css/personal/personal.css">
+    <!-- Integrazione Google Fonts -->
+    <!-- Logo -->
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
+    <!-- Menu orizzontale header -->
+    <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap" rel="stylesheet">
+    <!-- Testo home page main -->
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+    <!-- Testo prezzi servizi -->
+    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@500&display=swap" rel="stylesheet">
+	<!-- Banner IUBENDA -->
+	<script type="text/javascript">
+var _iub = _iub || [];
+_iub.csConfiguration = {"cookiePolicyInOtherWindow":true,"askConsentAtCookiePolicyUpdate":true,"whitelabel":false,"lang":"it","siteId":1552736,"cookiePolicyId":56591471, "banner":{ "slideDown":false,"acceptButtonDisplay":true,"customizeButtonDisplay":true,"acceptButtonColor":"#707b7c","acceptButtonCaptionColor":"white","customizeButtonColor":"#212121","customizeButtonCaptionColor":"white","position":"float-top-center","textColor":"#17202a","backgroundColor":"#d6dbdf","content":"<div id=\"iubenda-cs-title\">Informativa</div><div id=\"iubenda-cs-paragraph\">Io ed alcuni partner selezionati utilizziamo cookie o tecnologie simili come specificato nella <a href=\"/privacy-policy/56591471/cookie-policy?an=no&s_ck=false&newmarkup=yes\" class=\"iubenda-cs-cookie-policy-lnk\">cookie policy</a>.<br />Puoi acconsentire all’utilizzo di tali tecnologie chiudendo questa informativa, proseguendo la navigazione di questa pagina,  interagendo con un link o un pulsante al di fuori di questa informativa o continuando a navigare in altro modo.</div>" }};
+</script>
+<script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+  </head>
+  <body>
+    <!-- Inizio header -->
+    <header><?php include('./../template/header.php'); ?></header>
+    <!-- Fine header -->
+    <!-- Inizio main -->
+    <main>
+      <div class="container">
+        <!-- Inizio titolo homepage -->
+        <div class="row">
+          <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-12 col-xs-offset-0 text-center" id="main-div-home-page">
+            <h1 class="main-h1-home-page">Il tuo ordine è stato confermato.</h1>
+          </div>
+        </div>
+	</div>
+        <!-- Fine titolo homepage -->               
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-12 col-xs-offset-0 text-center">
+            <p class="description-price-service">Un sistema automatico ha trasmesso una e-mail di conferma all'indirizzo di posta elettronica da te fornito. Riceverai, nel più breve tempo possibile, i dettagli per procedere con il pagamento.<br><br>Ti ricordo che il sistema di ricezione degli ordini è perennemente attivo; l'effettiva presa in carico e la successiva evasione dell'ordine, non è garantito al di fuori dell'orario di ufficio (dal lunedì al venerdì dalle 09:00 alle 18:00), nei giorni festivi (sabato e domenica) e durante le principali festività.<br><br><b>ATTENZIONE</b><br>Se hai effettuato questo ordine per sbaglio contattami immediatamente: insieme troveremo una soluzione.<br><br><b>Grazie per aver scelto il mio servizio.</b></p>
+          </div>
+        </div>
+      </div>      
+      <br>
+	  <br>
+    </main>
+    <!-- Fine main -->
+    <!-- Inizio footer -->
+    <footer><?php require('./../template/footer.php') ?></footer>
+    <!-- Fine footer -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="./../js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+    <script async type="text/javascript" src="https://userlike-cdn-widgets.s3-eu-west-1.amazonaws.com/1d7b862aa74aa906fa0a8893f1c40ae214ba1128cbb36eb98becfe42868cdb82.js"></script>
+  </body>
+</html>
