e395cd6a976e9859687eeeba4acc8c535836b582
AlessioMozzino <82084197+AlessioMozzino@users.noreply.github.com>
2024-03-03 17:25:19 +0100

    Add files via upload

---
+++ legale/condizioni-vendita.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-133909830-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-133909830-1');
+</script>
+<link rel="apple-touch-icon" sizes="57x57" href="https://www.mozzino.it/favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="https://www.mozzino.it/favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="https://www.mozzino.it/favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="https://www.mozzino.it/favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="https://www.mozzino.it/favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="https://www.mozzino.it/favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="https://www.mozzino.it/favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="https://www.mozzino.it/favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="https://www.mozzino.it/favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="https://www.mozzino.it/favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://www.mozzino.it/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="https://www.mozzino.it/favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://www.mozzino.it/favicon/favicon-16x16.png">
+<link rel="manifest" href="https://www.mozzino.it/favicon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="https://www.mozzino.it/favicon/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Condizioni di vendita del servizio di recupero dei documenti catastali</title>
+		<meta name="keywords" content="documenti catastali, catasto, planimetria catastale, visura planimetrica, visura per immobile, estratto di mappa, visura storica, sister, agenzia delle entrate, visura catastale online, planimetria catastale online, mappa catastale, mappa, agenzia del territorio">
+		<meta name="description" content="Le condizioni di vendita hanno un solo obiettivo: tutelare nel loro percorso, attraverso semplici linee guida, il cliente ed il fornitore.">
+		<!-- Fogli di stile bootstrap -->
+		<link rel="stylesheet" type="text/css" href="./../css/bootstrap/bootstrap.min.css">
+		<!-- Fogli di stile personali -->
+		<link rel="stylesheet" type="text/css" href="./../css/personal/personal.css">
+		<!-- Integrazione Google Fonts -->
+		<!-- Logo -->
+		<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
+		<!-- Menu orizzontale header -->
+		<link href="https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap" rel="stylesheet">
+		<!-- Testo home page main -->
+		<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+		<!-- Testo prezzi servizi -->
+		<link href="https://fonts.googleapis.com/css2?family=Lora:wght@500&display=swap" rel="stylesheet">
+		<script type="text/javascript">
+var _iub = _iub || [];
+_iub.csConfiguration = {"cookiePolicyInOtherWindow":true,"askConsentAtCookiePolicyUpdate":true,"whitelabel":false,"lang":"it","siteId":1552736,"cookiePolicyId":56591471, "banner":{ "slideDown":false,"acceptButtonDisplay":true,"customizeButtonDisplay":true,"acceptButtonColor":"#707b7c","acceptButtonCaptionColor":"white","customizeButtonColor":"#212121","customizeButtonCaptionColor":"white","position":"float-top-center","textColor":"#17202a","backgroundColor":"#d6dbdf","content":"<div id=\"iubenda-cs-title\">Informativa</div><div id=\"iubenda-cs-paragraph\">Io ed alcuni partner selezionati utilizziamo cookie o tecnologie simili come specificato nella <a href=\"/privacy-policy/56591471/cookie-policy?an=no&s_ck=false&newmarkup=yes\" class=\"iubenda-cs-cookie-policy-lnk\">cookie policy</a>.<br />Puoi acconsentire all’utilizzo di tali tecnologie chiudendo questa informativa, proseguendo la navigazione di questa pagina,  interagendo con un link o un pulsante al di fuori di questa informativa o continuando a navigare in altro modo.</div>" }};
+</script>
+<script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+		<script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
+	</head>
+	<body>
+	<!-- Inizio header -->
+		<header><?php include('./../template/header.php'); ?></header>
+		<!-- Fine header -->
+		<!-- Inizio main -->
+		<main>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-12 text-justify" id="main-div-home-page">
+						<h1 class="main-h1-home-page">Condizioni generali di utilizzo e di vendita.</h1>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-12 col-xs-offset-0 text-justify">
+						<h4 class="form-acquista-titolo-settore">Premesse</h4>
+						<h5 class="form-acquisto-h5-guarda-esempio-documento" style="line-height: 1.5;">Il sito <b>www.mozzino.it</b> ed i servizi in esso contenuti sono offerti da <b>Alessio Mozzino</b>, libero professionista con sede legale in Giustenice (SV), in Piazza Vittorio Veneto civico 2, sede operativa in Borgio Verezzi (SV), in Via Giacomo Matteotti civico 173, codice fiscale MZZLSS96M30G605I, partita IVA 01782640096, posta elettronica certificata alessio.mozzino@pec.it, di seguito indicato come <i>fornitore</i>.<br>
+						Cliccando sul tasto <b>ordina</b>, posto nelle pagine di acquisto dei servizi, e completando la procedura di acquisto, confermi di aver preso visione e di accettare completamente le seguenti <b>condizioni generali di utilizzo e di vendita</b>, di seguito <i>condizioni</i>.
+						</h5><br>
+						<h4 class="form-acquista-titolo-settore">Disciplina contrattuale</h4>
+						<h5 class="form-acquisto-h5-guarda-esempio-documento" style="line-height: 1.5;">Compilando i diversi moduli di richiesta presenti sul sito accetti che il rapporto tra te in qualità di utilizzatore ed il <i>fornitore</i> siano regolati dalle presenti <i>condizioni</i>. Ti raccomando pertanto, prima di effettuare un nuovo ordine, di leggere attentamente le seguenti <i>condizioni</i>.<br>
+						Il <i>fornitore</i> si riserva il diritto di modificare a proprio piacimento le seguenti <i>condizioni</i>. Salvo diversa eccezione, per ogni modifica alle presenti <i>condizioni</i>, il <i>fornitore</i> è tenuto ad informare i propri utenti, ossia coloro i quali hanno già effettuatto uno o più ordini.<br>
+						La compravendita di servizi come quelli forniti dal <i>fornitore</i>, effettuata attraverso mezzi elettronici, viene disciplinata dal D. Lgs. numero 70 del 09/04/2003 e dal D. Lgs. numero 206 del 06/09/2005.
+						</h5><br>
+						<h4 class="form-acquista-titolo-settore">Conclusione del contratto</h4>
+						<h5 class="form-acquisto-h5-guarda-esempio-documento" style="line-height: 1.5;">Ogni servizio richiesto attraverso gli appositi moduli presenti sul sito, ovvero attraverso comunicazione agli indirizzi di contatto presenti, ha valore di proposta contrattuale e presuppone la tua completa e totale conoscenza delle presenti <i>condizioni</i>.<br>
+						Per confermare la presa in carico della tua richiesta una procedura automatizzata ti invierà, all'indirizzo e-mail fornito in fase di ordinazione, una e-mail di conferma con su riportati: l'identificativo dell'ordine (ID) e la cifra massima da saldare in caso di totale evasione dell'ordine.	Il presente contratto si intenderà concluso al termine della procedura di pagamento, ossia con l'evasione dell'ordine.
+						</h5><br>
+						<h4 class="form-acquista-titolo-settore">Adempimenti contabili</h4>
+						<h5 class="form-acquisto-h5-guarda-esempio-documento" style="line-height: 1.5;">Dovendo far fronte ad una serie di adempimenti contabili e fiscali, vale a dire emissione di regolare fattura per ogni ordine evaso, parzialmente o totalmente, il <i>fornitore</i> è tenuto a richiedere, in fase di ordinazione, gli appositi dati fiscali, vale a dire: nome, cognome, ragione sociale e legale rappresentante (in caso di società), provincia, comune, indirizzo e numero civico, codice fiscale e partita IVA.<br>
+						Qualora i dati forniti dall'utente in fase di ordinazione non siano sufficienti ad emettere regolare fattura fiscale il <i>fornitore</i> è tenuto a richiedere ulteriori dati aggiuntivi.<br>
+						Il <i>fornitore</i> è tenuto alla raccolta dei dati fiscali di ogni utente che richieda un servizio, solamente per procedere con i relativi adempimenti contabili; il tutto verrà effettuato nel rispetto della privacy di ogni utente.<br>
+						Nel caso in cui, per diverse ragioni o motivazioni, i dati fiscali dell'utente che abbia già effettuato ordini sul sito subiscano delle variazioni, l'utente stesso è tenuto ad informare il <i>fornitore</i> affinchè vengano modificati; in questo modo i dati necessari per gli adempimenti contabili saranno sempre corretti ed aggiornati.<br>
+						I dati fiscali raccolti, rientrando nella categoria di dati sensibili, non dovranno essere ceduti a terzi nè tantomeno essere utilizzati per scopi commerciali.
+						</h5><br>
+						<h4 class="form-acquista-titolo-settore">Prezzi, modalità di pagamento e fatturazione</h4>
+						<h5 class="form-acquisto-h5-guarda-esempio-documento" style="line-height: 1.5;">I prezzi variano in funzione del servizio richiesto. Per maggiore trasparenza il <i>fornitore</i> è tenuto ad informare i propri utenti del prezzo di ogni singolo servizio mediante apposizione degli stessi. Per maggiori informazioni si rimanda alla pagina "Prezzi".<br>
+						Al <i>fornitore</i> è concessa la facoltà di variare i prezzi a proprio piacimento. Anche in questo caso, così come per la modifica delle seguenti <i>condizioni</i>, il <i>fornitore</i> è obbligato a comunicare ai propri utenti, vale a dire a coloro i quali hanno già effettuato uno o più ordini sul sito, le modifiche al prezzario.<br>
+						Come metodo di pagamento l'utente avrà la possibilità di scegliere tra il servizio PayPal, che permette l'acquisto di beni e/o servizi attraverso carte di debito, di credito, o bonifico da conto corrente, senza necessariamente essere cliente PayPal, oppure attraverso bonifico bancario.<br>
+						Il pagamento non sarà richiesto subito, ma solo dopo aver verificato che i documenti richiesti dall'utente, parzialmente o totalmente, siano reperibili. Non verrà richiesto alcun pagamento all'utente qualora un singolo servizio non sia evadibile.<br>
+						Al termine della procedura di pagamento, vale a dire ad evasione dell'ordine, verrà emessa regolare fattura fiscale.
+						</h5><br>
+						<h4 class="form-acquista-titolo-settore">Tributi catastali</h4>
+						<h5 class="form-acquisto-h5-guarda-esempio-documento" style="line-height: 1.5;">Per poter recuperare particolari documenti catastali, affidando l'incarico a me in qualità di intermediario, l'Agenzia delle Entrate richiede il pagamento di una somma di denaro a titolo di tributi catastali.<br>Tale importo verrà anticipato dal <i>fornitore</i> per recuperare quanto richiesto in fase di ordinazione dal cliente. Quest'ultimo sarà poi tenuto, nella fase di pagamento dell'ordine, al saldo della somma anticipata dal <i>fornitore</i>. Tutti le somme anticipate verranno poi chiaramente specificate in fase di fatturazione.
+						</h5><br>
+						<h4 class="form-acquista-titolo-settore">Dati catastali e scarico di responsabilità</h4>
+						<h5 class="form-acquisto-h5-guarda-esempio-documento" style="line-height: 1.5;">Al fine di evadere l'ordine richiesto dall'utente, il <i>fornitore</i> è tenuto a richiedere i seguenti dati catastali: sezioni urbane, fogli catastali, particelle catastali, subalterni catastali, provincia e comune di appartenenza dell'immobile o degli immobili per cui è richiesto il servizio.<br>
+						Questi dati verrano raccolti per la sola evasione dell'ordine in questione: non verrano ceduti a terzi nè tantomeno utilizzati per scopi di lucro.<br>
+						Il <i>fornitore</i> non si assume alcuna responsabilità sul risultato finale in fase di evasione dell'ordine in quanto, i documenti richiesti, saranno recuperati sulla base dei dati immessi dall'utente in fase di ordinazione.<br>
+						Secondo quanto imposto dall'Agenzia delle Entrate, in fase di ordinazione del servizio, l'utente è tenuto a dichiarare se lui stesso è titolare dei diritti di proprietà o di altro diritto reale di godimento (esempio: usufruttuario), ovvero delegato dallo stesso, dell'immobile o degli immobili per cui sono richiesti i servizi; dichiarando una delle due opzioni, l'utente si assume le responsabilità delle sue dichiarazioni e sarà tenuto a rispondere in prima persona nel caso di danni a cose e/o persone, esonerando completamente il <i>fornitore</i>.
+						</h5><br>
+						<h4 class="form-acquista-titolo-settore">Tempi di consegna</h4>
+						<h5 class="form-acquisto-h5-guarda-esempio-documento" style="line-height: 1.5;">Durante la fase di ordinazione del servizio all'utente sarà data la possibilità di scegliere l'orario entro il quale l'ordine dovrà essere evaso, totalmente o parzialmente a seconda della piena presenza o meno dei documenti richiesti.<br>
+						Il <i>fornitore</i> non garantisce tuttavia il pieno e totale rispetto delle tempistiche richieste dal cliente nel caso in cui: il numero di ordini da evadere sia eccessivo, i sistemi utilizzati dal <i>fornitore</i> per il recupero dei documenti siano in fase di manutenzione, motivi personali legati al <i>fornitore</i>, l'ordine venga effettuato al di fuori dell'orario di ufficio (vale a dire dal lunedì al venerdì dalle 09:00 alle 18:00), durante i giorni festivi (sabato e domenica) e durante le principali festività.<br>
+						Sarà facoltà del <i>fornitore</i> informare prontamente l'utente nel caso in cui i tempi di consegna previsti non possano essere rispettati previa comunicazione all'indirizzo e-mail fornito dall'utente in fase di ordinazione.
+						</h5><br>
+						<h4 class="form-acquista-titolo-settore">Annullamento di uno o più ordini</h4>
+						<h5 class="form-acquisto-h5-guarda-esempio-documento" style="line-height: 1.5;">Sarà facoltà dell'utente annullare uno o più ordini effettuati, anche per sbaglio, previa comunicazione al <i>fornitore</i> attraverso posta elettronica entro e non oltre un'ora dalla data di ordinazione.<br>
+						Nel caso in cui, per diversi motivi, la comunicazione di annullamente dell'ordine avvenga oltre il tempo limite fissato (1 ora), ovvero dopo aver ricevuto l'e-mail con la richiesta di pagamento, l'utente sarà tenuto comunque al versamento dell'importo totale.<br>
+						Questa misura è necessaria dovendo sostenere dei costi per poter reperire quanto richiesto in fase di ordinazione dall'utente.<br>Verranno inoltre considerati annullati gli ordini per i quali è stata inviata la richiesta di pagamento ma che, da parte dell'utente, non vi sia stato alcun riscontro per più di 7 giorni, a partire dalla data di scadenza dell'ordine stesso (data di ordinazione più tempo limite per l'evasione). 
+						<br><br>
+					</div>
+				</div>
+			</div>
+		</main>
+		<footer><?php require('./../template/footer.php') ?></footer>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="./../js/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+		<script async type="text/javascript" src="https://userlike-cdn-widgets.s3-eu-west-1.amazonaws.com/1d7b862aa74aa906fa0a8893f1c40ae214ba1128cbb36eb98becfe42868cdb82.js"></script>		
+	</body>
+</html>
