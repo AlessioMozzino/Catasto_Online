fe026fa88f85306a31942b6618b5080f13d7c337
AlessioMozzino <82084197+AlessioMozzino@users.noreply.github.com>
2024-03-03 17:27:56 +0100

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,43 @@
+<div class="container" id="footer-container" style="margin-bottom: 0px;">
+  <div class="row" id="footer-container-row">
+    <div class="col-lg-12 col-lg-offset-0 col-md-12 col-md-offset-0 col-sm-12 col-sm-offset-0 col-xs-10 col-xs-offset-1">
+      <h3 class="footer-mini-logo">Mozzino</h3>
+      <hr class="footer-hr">
+    </div>
+  </div>
+  <div class="row" style="margin-top: 10px;">
+    <div class="col-lg-4 col-lg-offset-0 col-md-5 col-md-offset-0 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1" id="footer-div-informazioni-fiscali">
+      <ul class="footer-ul-link-utili">
+        <li class="footer-li-link-utili-title">Sede operativa:</li>
+        <li class="footer-li-link-utili">Borgio Verezzi (SV), Via Giacomo Matteotti, 173</li>
+        <li class="footer-li-link-utili">Partita IVA 01782640096</li>
+      </ul>
+    </div>
+    <div class="col-lg-2 col-lg-offset-0 col-md-2 col-md-offset-0 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1" id="footer-div-link-utili-legale">
+      <ul class="footer-ul-link-utili">
+        <li class="footer-li-link-utili-title">Area legale</li>
+        <li class="footer-li-link-utili"><a class="link-footer" href="https://www.iubenda.com/privacy-policy/56591471/full-legal" target="_blank">Privacy policy</a></li>
+        <li class="footer-li-link-utili"><a class="link-footer" href="https://www.iubenda.com/privacy-policy/56591471/cookie-policy" target="_blank">Cookie policy</a></li>
+        <li class="footer-li-link-utili"><a class="link-footer" href="https://www.mozzino.it/legale/condizioni-vendita">Condizioni di vendita</a></li>
+      </ul>
+    </div>
+    <div class="col-lg-2 col-lg-offset-0 col-md-2 col-md-offset-0 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1" id="footer-div-link-utili-legale">
+      <ul class="footer-ul-link-utili">
+        <li class="footer-li-link-utili-title">Pagamenti elettronici</li>
+        <li class="footer-li-link-utili"><img class="footer-img-carte-pagamenti" src="https://www.mozzino.it/img/card/carta-visa.png" alt="visa"><img class="footer-img-carte-pagamenti" src="https://www.mozzino.it/img/card/carta-mastercard.png" alt="mastercard"></li>
+        <li class="footer-li-link-utili"><img class="footer-img-carte-pagamenti" src="https://www.mozzino.it/img/card/carta-paypal.png" alt="paypal"><img class="footer-img-carte-pagamenti" src="https://www.mozzino.it/img/card/carta-american-express.png" alt="american express"></li>
+      </ul>
+    </div>
+    <div class="col-lg-4 col-lg-offset-0 col-md-3 col-md-offset-0 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1" id="footer-div-link-utili-legale" style="text-align: justify;">
+      <ul class="footer-ul-link-utili">
+        <li class="footer-li-link-utili-title">Assistenza clienti</li>
+        <li class="footer-li-link-utili">Hai bisogno di assistenza? Scrivimi in chat, vedremo di risolvere insieme il tuo problema.</li>
+      </ul>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-12 col-lg-offset-0 col-md-12 col-md-offset-0 col-sm-12 col-sm-offset-0 col-xs-10 col-xs-offset-1 text-center" id="footer-div-copyright">
+      <p class="footer-copyright">Copyright © 2018-<?php echo date('Y'); ?> / Tutti i diritti riservati. </p>
+    </div>
+  </div>
+</div>
---
+++ template/header.php
@@ -0,0 +1,77 @@
+<div class="container" id="header-top-container">
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-left">
+      <span><a class="header-a-top-link" href="mailto:info@mozzino.it"><img src="https://img.icons8.com/material-two-tone/24/000000/new-post.png"/></a></span>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-right">
+      <span><a class="header-a-top-link" href="https://www.facebook.com/mozzino.it" target="_blank"><img src="https://img.icons8.com/material-two-tone/24/000000/facebook.png"/></a></span>
+    </div>
+  </div>
+  <hr class="header-hr-top">
+  <div class="row">
+    <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 text-left" id="header-div-logo">
+      <h1 id="header-h1-logo"> <a class="header-h1-link-home" href="https://www.mozzino.it/home">Mozzino</a></h1>
+    </div>
+    <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12 text-right" id="header-div-menu">
+      <ul class="header-ul-menu-tendina-primo-livello">
+        <li class="header-li-menu-tendina-primo-livello"><a class="header-menu-link-page-primo-livello" href="https://www.mozzino.it/contatti">Contatti</a></li>
+        <li class="header-li-menu-tendina-primo-livello"><a class="header-menu-link-page-primo-livello">Informazioni</a>
+          <div class="header-div-menu-secondo-livello">
+            <ul class="header-ul-menu-tendina-secondo-livello">
+              <li class="header-li-menu-tendina-secondo-livello"><a class="header-menu-link-page-secondo-livello" href="https://www.mozzino.it/informazioni#come-funziona">Come funziona</a></li>
+              <li class="header-li-menu-tendina-secondo-livello"><a class="header-menu-link-page-secondo-livello" href="https://www.mozzino.it/informazioni#metodi-pagamento">Metodi di pagamento</a></li>
+              <li class="header-li-menu-tendina-secondo-livello"><a class="header-menu-link-page-secondo-livello" href="https://www.mozzino.it/informazioni#prezzi">Prezzi</a></li>
+              <li class="header-li-menu-tendina-secondo-livello"><a class="header-menu-link-page-secondo-livello" href="https://www.mozzino.it/informazioni#preventivo">Preventivo</a></li>
+			  <li class="header-li-menu-tendina-secondo-livello"><a class="header-menu-link-page-secondo-livello" href="https://www.mozzino.it/informazioni#documenti">Documenti</a></li>
+              <li class="header-li-menu-tendina-secondo-livello"><a class="header-menu-link-page-secondo-livello" href="https://www.mozzino.it/home#recensioni">Cosa dicono di me</a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="header-li-menu-tendina-primo-livello"><a class="header-menu-link-page-primo-livello">Acquista</a>
+          <div class="header-div-menu-secondo-livello">
+            <ul class="header-ul-menu-tendina-secondo-livello">
+              <li class="header-li-menu-tendina-secondo-livello"><a class="header-menu-link-page-secondo-livello" href="https://www.mozzino.it/acquista/s/planimetria-catastale">Planimetria catastale</a></li>
+              <li class="header-li-menu-tendina-secondo-livello"><a class="header-menu-link-page-secondo-livello" href="https://www.mozzino.it/acquista/s/visura-ordinaria">Visura ordinaria</a></li>
+              <li class="header-li-menu-tendina-secondo-livello"><a class="header-menu-link-page-secondo-livello" href="https://www.mozzino.it/acquista/s/visura-storica">Visura storica</a></li>
+              <li class="header-li-menu-tendina-secondo-livello"><a class="header-menu-link-page-secondo-livello" href="https://www.mozzino.it/acquista/s/estratto-mappa">Estratto di mappa</a></li>
+              <li class="header-li-menu-tendina-secondo-livello"><a class="header-menu-link-page-secondo-livello" href="https://www.mozzino.it/acquista/m/documenti-multipli"><b>Documenti multipli</b></a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="header-li-menu-tendina-primo-livello"><a class="header-menu-link-page-primo-livello" href="https://www.mozzino.it/chi-sono">Chi sono</a></li>
+        <li class="header-li-menu-tendina-primo-livello"><a class="header-menu-link-page-primo-livello" href="https://www.mozzino.it/home">Home</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="row">
+    <br>
+    <!-- Menù per schermi piccoli -->
+    <div class="col-sm-12 col-xs-12">
+      <form>
+        <select class="form-control" name="header-select-menu" id="header-menu-dropdown">
+          <option value="" disabled selected>Menù</option>
+          <option value="https://www.mozzino.it/home">Home</option>
+          <option value="https://www.mozzino.it/chi-sono">Chi sono</option>
+          <option value="https://www.mozzino.it/acquista/s/planimetria-catastale">acquista / Planimetria catastale</option>
+          <option value="https://www.mozzino.it/acquista/s/visura-ordinaria">acquista / Visura ordinaria</option>
+          <option value="https://www.mozzino.it/acquista/s/visura-storica">acquista / Visura storica</option>
+          <option value="https://www.mozzino.it/acquista/s/estratto-mappa">acquista / Estratto di mappa</option>
+          <option value="https://www.mozzino.it/acquista/m/documenti-multipli"><b>acquista / Documenti multipli</b></option>
+          <option value="https://www.mozzino.it/informazioni#come-funziona">informazioni / Come funziona</option>
+          <option value="https://www.mozzino.it/informazioni#metodi-pagamento">informazioni / Metodi di pagamanto</option>
+          <option value="https://www.mozzino.it/informazioni#prezzi">informazioni / Prezzi</option>
+		  <option value="https://www.mozzino.it/informazioni#preventivo">informazioni / Preventivo</option>
+          <option value="https://www.mozzino.it/informazioni#documenti">informazioni / Documenti</option>
+          <option value="https://www.mozzino.it/home#recensioni">informazioni / Cosa dicono di me</option>
+          <option value="https://www.mozzino.it/contatti">Contatti</option>
+        </select>
+        <script type="text/javascript">
+          var urlmenu = document.getElementById('header-menu-dropdown');
+          urlmenu.onchange = function() {
+            window.open(this.options[this.selectedIndex].value, "_self");
+          };
+        </script>
+      </form>
+    </div>
+  </div>
+</div>
